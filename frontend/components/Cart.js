351c3046dde8d771c3580c66dd0fa52210ccc20a
DevanshSK <devanshs843@gmail.com>
2022-09-15 00:21:24 +0530

    Basic cart added

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,7 +1,41 @@
+import { useShopContext } from "../lib/context";
+import {
+  CartWrapper,
+  CartStyle,
+  Card,
+  CardInfo,
+  EmptyDiv,
+} from "../styles/CartStyles";
+import { FaShoppingCart } from "react-icons/fa";
+
 export default function Cart() {
+  const { cartItems } = useShopContext();
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <CartWrapper>
+      <CartStyle>
+        {cartItems.length < 1 && (
+          <EmptyDiv>
+            <h1>You have more shopping to do 😉</h1>
+            <FaShoppingCart />
+          </EmptyDiv>
+        )}
+        {cartItems.length >= 1 &&
+          cartItems.map((item) => {
+            return (
+              <Card>
+                <img
+                  src={item.image.data.attributes.formats.thumbnail.url}
+                  alt={item.title}
+                />
+                <CardInfo>
+                  <h3>{item.title}</h3>
+                  <h3>{item.price}</h3>
+                </CardInfo>
+              </Card>
+            );
+          })}
+      </CartStyle>
+    </CartWrapper>
   );
 }
